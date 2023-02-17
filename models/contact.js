05023a0ed6f7f0000b1b49494be085c091984515
Andela Rwanda <andelarwanda@Andelas-MBP-3.mshome.net>
2023-02-17 16:50:16 +0200

    contact email

--- models/contact.js
+++ models/contact.js
@@ -1,24 +1,22 @@
 const mongoose = require('mongoose');
 
 const contactSchema = new mongoose.Schema({
-    
-name: {
-type: String,
-required: true,
-},
-email: {
-type: String,
-required: true,
-unique: false
-},
-message: {
-type: String,
-required: true,
-},
-date: {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  date: {
     type: Date,
-    default: Date.now
-}
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('Contact', contactSchema);
